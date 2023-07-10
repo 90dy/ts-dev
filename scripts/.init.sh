a450de2b315ec8ac68547eed14c0047df12c0ef9
90dy <90dy@proton.me>
2023-07-10 09:46:25 +0200

    chore: initial commit

---
+++ scripts/.init.sh
@@ -0,0 +1,6 @@
+.init.sh
+#!/bin/bash
+set -o pipefail # get exit status from the last failed command on pipe
+set -o errexit # exit on fail
+set -o nounset # no unset variable
+set -o xtrace # trace what's executed
