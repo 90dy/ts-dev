a35373fcbfd4ec088dab9cf9f5f1fd9f1a1aeff9
90dy <90dy@proton.me>
2023-08-12 19:34:31 +0200

    feat: babel config
    Signed-off-by: 90dy <90dy@proton.me>

--- babel-config.js
+++ babel-config.js
@@ -1,6 +1,33 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
-    '@babel/preset-typescript',
-  ],
+const path = require('path');
+
+module.exports = (api) => {
+	api.cache(false);
+	return {
+		env: {
+			production: {
+				plugins: ['transform-remove-console'],
+			},
+		},
+		plugins: [
+			'transform-inline-environment-variables',
+			[
+				'module-resolver',
+				{
+					root: [process.cwd()],
+					extensions: ['.js', '.jsx', '.ts', '.tsx'],
+					alias: {
+						// permit to build libs with babel
+						'@idgarages/*': path.resolve(__dirname, '../../libs/*'),
+						'axios/lib/*': 'axios/lib/*',
+					},
+				},
+			],
+			['@babel/plugin-syntax-top-level-await'],
+			['@babel/plugin-proposal-private-methods', { loose: true }],
+			['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+			['@babel/plugin-proposal-class-properties', { loose: true }],
+			'@babel/plugin-proposal-export-namespace-from',
+			'@babel/plugin-proposal-logical-assignment-operators',
+		],
+	};
 };
