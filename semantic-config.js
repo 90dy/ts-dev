3caea1e47e49d4a65819ad7b26d51163ab5c58c6
90dy <90dy@proton.me>
2023-08-12 21:45:35 +0200

    fix: semantic release
    Signed-off-by: 90dy <90dy@proton.me>

--- semantic-config.js
+++ semantic-config.js
@@ -3,5 +3,11 @@
 /** @type {SemanticReleaseConfig} */
 module.exports = {
 	branches: ['main'],
-	plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator'],
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/changelog",
+		"@semantic-release/npm",
+		"@semantic-release/git",
+		"@semantic-release/github"
+	],
 }
